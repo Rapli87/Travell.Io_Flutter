7dec03533643ef42e581bd589bb95bbe03c9039a
yulianadewi18 <yulianadewi210@gmail.com>
2022-06-07 15:51:11 +0700

    app_bar tittle

--- travelio/lib/widgets/app_bar_title.dart
+++ travelio/lib/widgets/app_bar_title.dart
@@ -1,1 +1,33 @@
-(dewi)
+import 'package:flutter/material.dart';
+import 'package:travel_io/res/custom_colors.dart';
+
+class AppBarTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/41365931.png',
+          height: 20,
+        ),
+        SizedBox(width: 8),
+        Text(
+          'Travell',
+          style: TextStyle(
+            color: CustomColors.firebaseAmber,
+            fontSize: 18,
+          ),
+        ),
+        Text(
+          '.Io',
+          style: TextStyle(
+            color: CustomColors.firebaseOrange,
+            fontSize: 18,
+          ),
+        ),
+      ],
+    );
+  }
+}
