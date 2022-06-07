b6780c7dea35cf31eb2f9bad98000e1f1e52bda1
Rafli Al Fajar <75103755+Rapli87@users.noreply.github.com>
2022-06-07 19:07:50 +0700

    update menu.dart

--- travelio/lib/models/menu.dart
+++ travelio/lib/models/menu.dart
@@ -1,1 +1,25 @@
-(rafli)
+class Menu {
+  int id;
+  String image;
+  String name;
+  int price;
+  int pricePromo;
+  int price2;
+  int price4;
+  int pricetamb;
+  String note;
+  bool isPromo;
+
+  Menu({
+    required this.id,
+    required this.image,
+    required this.name,
+    required this.price,
+    required this.pricePromo,
+    required this.note,
+    required this.isPromo,
+    required this.price2,
+    required this.price4,
+    required this.pricetamb,
+  });
+}
