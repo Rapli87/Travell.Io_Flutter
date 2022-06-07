1f0463cc36e8a648f795d4e7b3d6f9967f3caee0
yulianadewi18 <yulianadewi210@gmail.com>
2022-06-07 15:52:44 +0700

    custom_colors

--- travelio/lib/res/custom_colors.dart
+++ travelio/lib/res/custom_colors.dart
@@ -1,1 +1,11 @@
-(dewi)
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static final Color firebaseNavy = Color.fromARGB(255, 1, 16, 66);
+  static final Color firebaseOrange = Color.fromARGB(255, 24, 137, 223);
+  static final Color firebaseAmber = Color.fromARGB(255, 255, 255, 255);
+  static final Color firebaseYellow = Color.fromARGB(255, 0, 0, 0);
+  static final Color firebaseGrey = Color.fromARGB(255, 255, 255, 255);
+  static final Color googleBackground = Color.fromARGB(255, 0, 0, 0);
+  static final Color googleBackground1 = Color(0xFF4285F4);
+}
