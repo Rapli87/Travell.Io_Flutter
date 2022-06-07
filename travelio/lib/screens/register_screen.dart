e82f112ec6072a2e7ca4c217cf51ce11db844577
yulianadewi18 <yulianadewi210@gmail.com>
2022-06-07 15:43:36 +0700

    Update register_screen.dart

--- travelio/lib/screens/register_screen.dart
+++ travelio/lib/screens/register_screen.dart
@@ -1,1 +1,59 @@
-(dewi)
+import 'package:flutter/material.dart';
+import 'package:travel_io/res/custom_colors.dart';
+import 'package:travel_io/widgets/app_bar_title.dart';
+import 'package:travel_io/widgets/register_form.dart';
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final FocusNode _nameFocusNode = FocusNode();
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _nameFocusNode.unfocus();
+        _emailFocusNode.unfocus();
+        _passwordFocusNode.unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: CustomColors.firebaseNavy,
+          title: AppBarTitle(),
+        ),
+        body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/30189.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 16.0,
+                right: 16.0,
+                bottom: 20.0,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 16.0,
+                  right: 16.0,
+                  bottom: 20.0,
+                ),
+                child: RegisterForm(
+                  nameFocusNode: _nameFocusNode,
+                  emailFocusNode: _emailFocusNode,
+                  passwordFocusNode: _passwordFocusNode,
+                ),
+              ),
+            )),
+      ),
+    );
+  }
+}
